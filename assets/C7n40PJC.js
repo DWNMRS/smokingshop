42c4a39886b9c6dc92eb263fe76741fba3adfdaa
DWNMRS <Downmars@yandex.ru>
2024-06-19 04:37:01 +0400

    Updates

--- assets/C7n40PJC.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./CEgHc7iE.js";import{D as o,x as t,H as c}from"./pcIdDT9R.js";const s={},n=c("h1",null,"Каталог",-1),r=[n];function a(_,l){return t(),o("div",null,r)}const i=e(s,[["render",a]]);export{i as default};
