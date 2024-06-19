edf5209eee3bf2193037f700701f992962f50aa1
DWNMRS <Downmars@yandex.ru>
2024-06-19 04:24:11 +0400

    Updates

--- _nuxt/C54IGMBt.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./4ba1aMIA.js";import{D as o,x as t,H as c}from"./4hnnTRhZ.js";const s={},n=c("h1",null,"Каталог",-1),r=[n];function a(_,l){return t(),o("div",null,r)}const i=e(s,[["render",a]]);export{i as default};
---
+++ assets/C95NmY36.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DgdM7Tel.js";import{D as o,x as t,H as c}from"./pcIdDT9R.js";const s={},n=c("h1",null,"Блог",-1),r=[n];function _(a,l){return t(),o("div",null,r)}const i=e(s,[["render",_]]);export{i as default};
